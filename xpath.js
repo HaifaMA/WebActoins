77cfff236e66e29442e1d0f7e98d63ae4d8ff533
Haifa Al <127701416+HaifaMA@users.noreply.github.com>
2023-10-21 15:29:59 -0700

    Add files via upload

--- xpath.js
+++ xpath.js
@@ -1,7 +1,8 @@
 const { expect } = require('chai');
 const { describe, it } = require("mocha");
-describe('Xpath strategies', () => { 
+describe('Advanced Xpath strategies', () => { 
     it('Verify current temp is less than or equals to feel-like temp', async () => {
+
         await browser.url('https://www.accuweather.com/');
 
         const currentTempElement = await $('//span[@class="recent-location-temp"]').getText();
@@ -18,9 +19,42 @@ describe('Xpath strategies', () => {
             console.log(`Current temperature is not between 45 and 55.`);
         }
     });
+
+    it('Verify error on empty login flow', async () => {
+
+        await browser.url('/');
+        await $('//button[@name="login"]').click();
+        const errorMsgDisplayed = await $('//div[contains(text(), "isn’t connected to an account")]').isDisplayed();
+        expect(errorMsgDisplayed, 'Error Message is NOT displayed').to.be.true;
+    });
+    
+    it('Verify the empty messenger login flow', async () => {
+    
+        await browser.url('/');
+
+        await $('//a[text()="Messenger"]').click();
+
+        const checkboxSelected = await $('//label[@class="uiInputLabelInput"]').isSelected();
+        expect(checkboxSelected, 'Checkbox is selected').to.be.false;
+
+        await browser.pause(2000);
+        await $('//button[@name="login"]').click();
+
+        const linkDisplayed = await $('//a[contains(text(), "Find your account")]').isDisplayed();
+        expect(linkDisplayed, 'Find your account link is NOT displayed').to.be.true;
+
+        const continueBtnEnabled = await $('//button[@id="loginbutton"]').isEnabled();
+        expect(continueBtnEnabled, 'Continue button is NOT enabled').to.be.true;
+
+        const signInCheckbox = await $('//label[@class="uiInputLabelInput"]');
+        const signInSelected = await signInCheckbox.isSelected()
+        expect(signInSelected, 'Sign in checkbox is selected').to.be.false;
+
+        await signInCheckbox.click();
+
+        const signInAfterCheck = await $('//input[@name="persistent"]');
+        const signInAfterSelect = await signInAfterCheck.isSelected();
+        expect(signInAfterSelect, 'Sign in checkbox is NOT selected').to.be.true;
+    });
 });
-
-
-
-
 
